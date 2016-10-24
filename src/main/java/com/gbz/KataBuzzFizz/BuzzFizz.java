8dde109ccc990f518e9df3870f0051063ecc2135
Cedric GIBOULOT <cedric.giboulot@bnpparibas.com>
2016-10-24 17:17:55 +0200

    initialisation du projet

---
+++ src/main/java/com/gbz/KataBuzzFizz/BuzzFizz.java
@@ -0,0 +1,12 @@
+package com.gbz.KataBuzzFizz;
+
+/**
+ * Hello world!
+ *
+ */
+public class BuzzFizz 
+{
+
+	
+	
+}
---
+++ src/test/java/com/gbz/KataBuzzFizz/BuzzFizzTest.java
@@ -0,0 +1,25 @@
+package com.gbz.KataBuzzFizz;
+
+import static org.hamcrest.CoreMatchers.allOf;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class BuzzFizzTest 
+{
+     
+	@Test
+	public void test(){
+		assertThat(true, is(true));
+	}
+}
